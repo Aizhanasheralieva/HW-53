02c2795e208799c50f26b4e9d8cd46aa2a4fa9cb
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-09-19 15:35:49 +0600

    Made some corrections in AddTaskForm.tsx

--- src/Components/AddTaskForm.tsx
+++ src/Components/AddTaskForm.tsx
@@ -1,11 +1,6 @@
 const AddTaskForm = ({currentTask, setCurrentTask, addTask}) => (
   <div className="add-task-form">
-    <input
-      type="text"
-      value={currentTask}
-      onChange={(event) => setCurrentTask(event.target.value)}
-      placeholder="Add new task"
-    />
+    <input type="text" value={currentTask} onChange={(event) => setCurrentTask(event.target.value)} placeholder="Add new task"/>
     <button onClick={addTask}>Add</button>
   </div>
 );
